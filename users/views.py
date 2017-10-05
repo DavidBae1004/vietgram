7cf8c42efae733ca266d78c24cd379085eb76046
DavidBae1004 <DavidBae1004@users.noreply.github.com>
2017-10-05 13:59:45 +0900

    Urls, index and login view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
-# Create your views here.
+
+def index(request) :
+    if request.user.is_authentificated():
+        response = HttpResponse ('Feed')
+    else:
+        response = HttpResponseRedirect(reverse('login'))
+    return reponse
+
+
+
+def login(request)
+    if not request.user.is_authentificated():
+        response = HttpResponse ('Log in')
+    else:
+        response = HttpResponseRedirect(reverse('index'))
+    return reponse
